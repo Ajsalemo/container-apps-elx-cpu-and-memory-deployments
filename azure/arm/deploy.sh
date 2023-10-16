28ff52e7c55430c0654bad6c7e7f11972cfe2c51
Anthony Salemo <ajssalemo@gmail.com>
2023-10-16 11:12:20 -0400

    doing additional changes to include memory

--- azure/arm/deploy.sh
+++ azure/arm/deploy.sh
@@ -1,6 +1,6 @@
 # Run the following command - relative to deploy.json
 # Make sure to change the location if desired
-# After the initial deployment, Change the CPU values as seen fit. Make sure this matches the allowed CPU <-> Memory combinations as seen here: https://learn.microsoft.com/en-us/azure/container-apps/containers#configuration
+# After the initial deployment, Change the CPU and memory values as seen fit. Make sure this matches the allowed CPU <-> Memory combinations as seen here: https://learn.microsoft.com/en-us/azure/container-apps/containers#configuration
 # eg., 0.75 CPU and 1.5Gi memory
 
-az deployment group create --resource-group "ansalemo-rg" --template-file deploy.json --parameters location="eastus"
+az deployment group create --resource-group "your-rg" --template-file deploy.json --parameters location="eastus"
--- azure/azure-cli/deploy.sh
+++ azure/azure-cli/deploy.sh
@@ -1,5 +1,5 @@
 # Run the following command to update an existing Container App
-# Change the CPU values as seen fit. Make sure this matches the allowed CPU <-> Memory combinations as seen here: https://learn.microsoft.com/en-us/azure/container-apps/containers#configuration
+# Change the CPU and memory values as seen fit. Make sure this matches the allowed CPU <-> Memory combinations as seen here: https://learn.microsoft.com/en-us/azure/container-apps/containers#configuration
 # eg., 0.75 CPU and 1.5Gi memory
 
-az containerapp update -n ansalemo-cpu -g ansalemo-rg --cpu 0.75 --memory 1.5
+az containerapp update -n test-app -g your-rg --cpu 0.75 --memory 1.5
